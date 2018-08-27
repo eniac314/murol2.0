a16c31e2cb988032ffbaf160320ec09e664e6b83
fayong <eniac314@gmail.com>
2018-08-27 13:17:46 +0200

    --

---
+++ src/Icons.elm
@@ -0,0 +1,57 @@
+module Icons exposing (..)
+
+import Html exposing (Html)
+import Svg exposing (Svg, svg)
+import Svg.Attributes exposing (..)
+
+
+svgFeatherIcon : String -> List (Svg msg) -> Html msg
+svgFeatherIcon className =
+    svg
+        [ class <| "feather feather-" ++ className
+        , fill "none"
+        , height "24"
+        , stroke "currentColor"
+        , strokeLinecap "round"
+        , strokeLinejoin "round"
+        , strokeWidth "2"
+        , viewBox "0 0 24 24"
+        , width "24"
+        ]
+
+
+link2 : Html msg
+link2 =
+    svgFeatherIcon "link-2"
+        [ Svg.path [ d "M15 7h3a5 5 0 0 1 5 5 5 5 0 0 1-5 5h-3m-6 0H6a5 5 0 0 1-5-5 5 5 0 0 1 5-5h3" ] []
+        , Svg.line [ x1 "8", y1 "12", x2 "16", y2 "12" ] []
+        ]
+
+
+externalLink : Html msg
+externalLink =
+    svgFeatherIcon "external-link"
+        [ Svg.path [ d "M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" ] []
+        , Svg.polyline [ points "15 3 21 3 21 9" ] []
+        , Svg.line [ x1 "10", y1 "14", x2 "21", y2 "3" ] []
+        ]
+
+
+list : Html msg
+list =
+    svgFeatherIcon "list"
+        [ Svg.line [ x1 "8", y1 "6", x2 "21", y2 "6" ] []
+        , Svg.line [ x1 "8", y1 "12", x2 "21", y2 "12" ] []
+        , Svg.line [ x1 "8", y1 "18", x2 "21", y2 "18" ] []
+        , Svg.line [ x1 "3", y1 "6", x2 "3", y2 "6" ] []
+        , Svg.line [ x1 "3", y1 "12", x2 "3", y2 "12" ] []
+        , Svg.line [ x1 "3", y1 "18", x2 "3", y2 "18" ] []
+        ]
+
+
+bold : Html msg
+bold =
+    svgFeatherIcon "bold"
+        [ Svg.path [ d "M6 4h8a4 4 0 0 1 4 4 4 4 0 0 1-4 4H6z" ] []
+        , Svg.path [ d "M6 12h9a4 4 0 0 1 4 4 4 4 0 0 1-4 4H6z" ] []
+        ]
---
+++ src/TextBlock.elm
@@ -0,0 +1,205 @@
+module TextBlock exposing (..)
+
+--import Document exposing (..)
+
+import Browser exposing (element)
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events as Events
+import Element.Font as Font
+import Element.Input as Input
+import Element.Lazy as Lazy
+import Element.Region as Region
+import Html exposing (Html, i, node, textarea)
+import Html.Attributes as Attr
+import Html.Events as HtmlEvents
+import Icons exposing (..)
+import Json.Decode as Decode
+
+
+type alias DocTextBlock =
+    { rawInput : String
+    , selected : Maybe Selection
+    , nbrCols : Maybe Int
+    }
+
+
+type alias TextBlock =
+    List TextBlockElement
+
+
+type TextBlockElement
+    = Paragraph (List TextBlockElement)
+    | Link LinkMeta
+    | Bold String
+    | UList (List TextBlockElement)
+
+
+type alias LinkMeta =
+    { targetBlank : Bool
+    , url : String
+    , label : String
+    }
+
+
+type Msg
+    = NoOp
+    | NewSelection Selection
+    | ClearSelection
+    | TextInput String
+    | Loaded String
+
+
+type alias Selection =
+    { start : Int
+    , finish : Int
+    , sel : String
+    }
+
+
+main : Program () DocTextBlock Msg
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+subscriptions model =
+    Sub.none
+
+
+init flags =
+    ( { rawInput = ""
+      , selected = Nothing
+      , nbrCols = Nothing
+      }
+    , Cmd.none
+    )
+
+
+update msg model =
+    case msg of
+        TextInput s ->
+            ( { model | rawInput = s }
+            , Cmd.none
+            )
+
+        NewSelection s ->
+            ( { model
+                | selected =
+                    if s.start == s.finish then
+                        Nothing
+                    else
+                        Just s
+              }
+            , Cmd.none
+            )
+
+        ClearSelection ->
+            ( { model | selected = Nothing }
+            , Cmd.none
+            )
+
+        Loaded n ->
+            ( { model | nbrCols = String.toInt n }
+            , Cmd.none
+            )
+
+        NoOp ->
+            ( model, Cmd.none )
+
+
+view model =
+    Element.layout [] <|
+        column
+            [ padding 15
+            , spacing 15
+            ]
+            [ interfaceView model
+            , customTextArea
+                [ width fill ]
+            , paragraph [ width (maximum 500 fill) ]
+                [ text <| Debug.toString model ]
+            ]
+
+
+interfaceView model =
+    row [ spacing 10 ]
+        [ Input.button buttonStyle
+            { onPress = Nothing
+            , label = html <| link2
+            }
+        , Input.button
+            buttonStyle
+            { onPress = Nothing
+            , label = html <| externalLink
+            }
+        , Input.button buttonStyle
+            { onPress = Nothing
+            , label = html <| bold
+            }
+        , Input.button
+            buttonStyle
+            { onPress = Nothing
+            , label = html <| list
+            }
+        ]
+
+
+textBlocPreview model =
+    column []
+        []
+
+
+customTextArea attrs =
+    el attrs
+        (html <|
+            node "custom-textarea"
+                [ HtmlEvents.onInput TextInput
+                , HtmlEvents.on "Selection" decodeSelection
+                , HtmlEvents.on "Loaded" decodeLoaded
+                ]
+                [ textarea
+                    [ --Attr.style "resize" "none"
+                      Attr.style "font-family" "Arial"
+                    , Attr.style "font-size" "16px"
+                    , Attr.cols 60
+                    , Attr.style "height" "500px"
+                    , Attr.style "spellcheck" "false"
+                    ]
+                    []
+                ]
+        )
+
+
+
+--computeHeight
+
+
+decodeSelection =
+    Decode.at [ "target", "selection" ]
+        (Decode.map3 Selection
+            (Decode.field "start" Decode.int)
+            (Decode.field "finish" Decode.int)
+            (Decode.field "sel" Decode.string)
+            |> Decode.map NewSelection
+        )
+
+
+decodeLoaded =
+    Decode.map Loaded <|
+        Decode.at [ "target", "cols" ] Decode.string
+
+
+buttonStyle =
+    [ Border.rounded 5
+    , Font.center
+    , centerY
+    , paddingXY 5 3
+    , mouseOver
+        [ Background.color (rgb 0.9 0.9 0.9) ]
+    ]
